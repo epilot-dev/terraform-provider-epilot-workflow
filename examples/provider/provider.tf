f7a1ea765a5316bae7a12cb6cd48c8cdecccec7d
Hans Weber <hans.weber@epilot.cloud>
2023-11-11 15:47:05 +0100

    chore: generate without speakeasy annotations

---
+++ examples/provider/provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    epilot-product = {
+      source  = "epilot-dev/epilot-product"
+      version = "0.2.0"
+    }
+  }
+}
+
+provider "epilot-product" {
+  # Configuration options
+}
