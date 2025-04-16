d49119889a5a2e0963f7e79550c4b01120e080b1
Nishu Goel <n.goel@epilot.cloud>
2025-04-16 15:00:56 +0530

    feat: Support flows in blueprints

--- examples/test/gene.tf
+++
@@ -1,8 +0,0 @@
-# epilot-workflow_closing_reason.my_closingreason:
-resource "epilot-workflow_closing_reason" "my_closingreason" {
-    # creation_time    = "2023-06-23T09:00:51.120Z"
-    # id               = "t3_s3j9zBAhxOGbTUa03e"
-    # last_update_time = "2023-06-23T09:00:51.120Z"
-    status           = "ACTIVE"
-    title            = "Zu Teuer"
-}
